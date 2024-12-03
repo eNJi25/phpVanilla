8809399f4024c1eda55a90c084ec796d8ce879f1
eNJi25 <nfischer.contact@gmail.com>
2024-12-03 09:42:16 +0100

    add footer and header in templates and bootstrap cdn

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'templates/header.php';
+?>
+
+<div class="col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+            <img src="assets/images/logo-okaz.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        </div>
+        <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Avec Okaz achetez et vendez vos objets</h1>
+            <p class="lead">Trouvez ce que vous cherchez ou donnez une seconde vie à vos objets en un clic ! Okaz est la plateforme incontournable pour vendre, acheter ou échanger tout ce que vous souhaitez : vêtements, meubles, jeux vidéos et bien plus encore !</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once 'templates/footer.php';
+?>
